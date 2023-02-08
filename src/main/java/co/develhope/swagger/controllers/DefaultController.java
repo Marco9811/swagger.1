93c72341f973d7a9fd20792f01aad718613ad752
Marco Abbondanza <107813503+Marco9811@users.noreply.github.com>
2023-02-08 18:09:52 +0100

    Update DefaultController.java

--- src/main/java/co/develhope/swagger/controllers/DefaultController.java
+++ src/main/java/co/develhope/swagger/controllers/DefaultController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class DefaultController {
     @GetMapping
     public String welcome(){
-        return "Welcome to the show!";
+        return "Welcome";
     }
 }
